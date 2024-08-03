8bc79b06199f3295a2251d60c1ad16aa87090fc3
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-03 12:29:19 +0530

    fixed some types in csvFileReader class

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,42 +1,17 @@
 import fs from 'node:fs';
 
-enum MatchResult {
-  HomeTeamWon = 'H',
-  AwayTeamWon = 'A',
-  Draw = 'D',
-}
-
 export class CsvFileReader {
   filename: string;
-  data: string[][];
+  data: string[][] = [];
 
   constructor(filename: string) {
     this.filename = filename;
+  }
+
+  readFile(): void {
     this.data = fs
       .readFileSync(this.filename, 'utf-8')
       .split('\n')
       .map(match => match.split(','));
-  }
-
-  result(): void {
-    let manUnitedWinCount: number = 0;
-
-    for (const match of this.data) {
-      if (
-        match.at(1) === 'Man United' &&
-        match.at(5) === MatchResult.HomeTeamWon
-      ) {
-        manUnitedWinCount++;
-      }
-
-      if (
-        match.at(2) === 'Man United' &&
-        match.at(5) === MatchResult.AwayTeamWon
-      ) {
-        manUnitedWinCount++;
-      }
-    }
-
-    console.log(manUnitedWinCount);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
 import { CsvFileReader } from './CsvFileReader';
 
 const reader = new CsvFileReader('data/football.csv');
-reader.result();
+reader.readFile();
+
+enum MatchResult {
+  HomeTeamWon = 'H',
+  AwayTeamWon = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWinCount: number = 0;
+
+for (const match of reader.data) {
+  if (match.at(1) === 'Man United' && match.at(5) === MatchResult.HomeTeamWon) {
+    manUnitedWinCount++;
+  }
+
+  if (match.at(2) === 'Man United' && match.at(5) === MatchResult.AwayTeamWon) {
+    manUnitedWinCount++;
+  }
+}
+
+console.log(manUnitedWinCount);
