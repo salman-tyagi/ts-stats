50be0e6b091f38186963a24e8a8127ff91788f89
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-05 14:31:08 +0530

    read data from csv file using interfaces and second refactor

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('data/football.csv');
-reader.readFile();
+const csvFileReader = new CsvFileReader('data/football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWinCount: number = 0;
 
-for (const match of reader.data) {
+for (const match of matchReader.matches) {
   if (match.at(1) === 'Man United' && match.at(5) === MatchResult.HomeTeamWon) {
     manUnitedWinCount++;
   }
--- src/inheritance/CsvFileReader copy.ts
+++ src/inheritance/CsvFileReader copy.ts
@@ -1,9 +1,11 @@
 import fs from 'node:fs';
 
-export class CsvFileReader {
-  data: string[][] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   readFile(): void {
     this.data = fs
@@ -11,6 +13,7 @@ export class CsvFileReader {
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
-      });
+      })
+      .map(this.mapRow);
   }
 }
