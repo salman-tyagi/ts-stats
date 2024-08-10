4fd9ba150fce433b3ce64034108ac42138b44ec1
Salman Tyagi <salmantyagi15@gmail.com>
2024-08-10 15:46:37 +0530

    generate htmlReport.html file using class composition

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import CsvFileReader from './CsvFileReader';
 import WinsAnalyzer from './analyzers/WinsAnalyzer';
 import ConsoleReport from './reportTargets/ConsoleReport';
 import Summary from './Summary';
+import HtmlReport from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('data/football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const analyzer = new WinsAnalyzer('Man United');
+const analyzer = new WinsAnalyzer('Crystal Palace');
 const consoleReport = new ConsoleReport();
+const htmlReport = new HtmlReport();
 
-const summary = new Summary(analyzer, consoleReport);
+// const summary = new Summary(analyzer, consoleReport);
+const summary = new Summary(analyzer, htmlReport);
 summary.buildAndBuildReport(matchReader.matches);
